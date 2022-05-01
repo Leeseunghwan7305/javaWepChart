19d3c0889e4719fc43ccf745e84e7c16334f3387
Leeseunghwan7305 <sun1238799@naver.com>
2022-05-01 14:14:58 +0900

    [add] tap menu

--- client/src/component/Nav/Nav.js
+++ client/src/component/Nav/Nav.js
@@ -1,16 +1,63 @@
 import React, { useState } from "react";
 import "./Nav.css";
+import { FiAlignJustify } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 const Nav = ({ setChartType }) => {
+  const [toggleBox, setToggleBox] = useState(false);
+  const [selectBox, setSelectBox] = useState("");
   function changeType(type) {
     setChartType(type);
   }
+  function toggle(e) {
+    if (
+      e.target.classList[0] === "buttons" ||
+      e.target.classList[0] === "fixIcons" ||
+      e.target.classList[0] === "icons"
+    )
+      setToggleBox(!toggleBox);
+  }
+  function colorBox(e) {
+    console.log(e.target.innerHTML);
+    setSelectBox(e.target.innerHTML);
+  }
   return (
-    <div className="nav">
-      <button onClick={() => changeType("radar")}>레이더</button>
-      <button onClick={() => changeType("line")}>라인</button>
-      <button onClick={() => changeType("bar")}>바</button>
-      <button onClick={() => changeType("horizontalBar")}>수평바</button>
-    </div>
+    <>
+      <div className="nav">
+        <FiAlignJustify className="icons" onClick={toggle}></FiAlignJustify>
+      </div>
+      <div
+        onClick={toggle}
+        className={`buttons ${toggleBox ? "ToggleButton" : null}`}
+      >
+        <div className="buttonTap" onClick={colorBox}>
+          <FiX className="fixIcons"></FiX>
+          <button
+            className={selectBox === "레이더" ? "clickBox" : null}
+            onClick={() => changeType("radar")}
+          >
+            레이더
+          </button>
+          <button
+            className={selectBox === "라인" ? "clickBox" : null}
+            onClick={() => changeType("line")}
+          >
+            라인
+          </button>
+          <button
+            className={selectBox === "바" ? "clickBox" : null}
+            onClick={() => changeType("bar")}
+          >
+            바
+          </button>
+          <button
+            className={selectBox === "수평바" ? "clickBox" : null}
+            onClick={() => changeType("horizontalBar")}
+          >
+            수평바
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
